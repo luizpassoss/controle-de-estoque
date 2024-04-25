004f1af9897a7d710dee2c69093bfcd891d50623
luizpassoss <luiz182004@gmail>
2024-04-25 20:04:09 -0300

    feito

---
+++ Login/login.js
@@ -0,0 +1,8 @@
+// Lógica para lidar com o clique no link "Forgot your password?"
+document.getElementById("forgotPassword").addEventListener("click", function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do link (navegar para outra página)
+  
+    // Implemente a lógica de recuperação de senha aqui
+    // Por exemplo, exibir um formulário de recuperação de senha modal ou redirecionar para uma página de recuperação de senha
+    alert("Implemente a lógica de recuperação de senha aqui.");
+  });
